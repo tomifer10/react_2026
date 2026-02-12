d11326c67c2ed330d284ab6a59cd814ea81ba6f3
Tomas Fernandez <105197757+tomifer10@users.noreply.github.com>
2026-02-12 13:24:00 +0100

    bases

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,11 @@
+const firstName = "Tomas";
+const secondName = "Fernandez";
+
+let diceNumber = 5;
+diceNumber = 3;
+
+console.log(firstName, secondName);
+
+const containLetterF = secondName.includes("F");
+
+console.log({ containLetterF });
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import "./style.css";
+import "./bases/01-const-let";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
    <h1> Hola Mundo </h1>
   </div>
 `;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
